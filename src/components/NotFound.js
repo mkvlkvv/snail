c1a8278b29c66ce391fac14def08912abb7a4ea0
mariya.kovalkova <MARIYA.KOVALKOVA@ERIELL.COM>
2024-04-26 17:19:00 +0300

    404

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,24 +4,24 @@ import VideoPlayer from 'react-video-js-player';
 import poster from '../images/not_found.png'
 //https://www.youtube.com/watch?v=8f7lRUrZY-Q&ysclid=lvgnul6deu156583303
 
-/*<div class="not_found__number">
-                404
-            </div>
-            <div class="not_found__desc">
-                <p>Страница не найдена</p>
-                <Link to="/snailweb">
-                    <div class="not_found__to">
-                        На главную
-                    </div>
-                </Link>
-
-            </div> */
+/* */
 
 const NotFound = () => {
     
     return(
         <div class="not_found__container">
-            <iframe margin-left="800px" width="560" height="315" src="https://www.youtube.com/embed/8f7lRUrZY-Q?si=87x-0ZnZNeskdMVW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div class="not_found__number">
+                <a>404</a>
+            </div>
+            <div class="not_found__desc">
+                <p>Страница не найдена</p>
+                <Link class="not_found__link" to="/snailweb">
+                    <div class="not_found__to">
+                        <a>На главную</a>
+                    </div>
+                </Link>
+
+            </div>
 
         </div>
     )
